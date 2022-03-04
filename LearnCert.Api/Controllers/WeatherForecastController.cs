30681defc2af3ccdf6de14899e5ea2752fbddad5
mcosta21 <marcioc424@gmail.com>
2022-03-03 21:28:21 -0300

    Removing bin and obj folders

--- LearnCert.Api/Controllers/WeatherForecastController.cs
+++ LearnCert.Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace LearnCert.Api.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Hello", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
