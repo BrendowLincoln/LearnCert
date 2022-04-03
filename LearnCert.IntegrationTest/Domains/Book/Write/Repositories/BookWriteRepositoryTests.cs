7c0d3b3a1e0be304fba9dfeeece38ff6e5c842cc
mcosta21 <marcioc424@gmail.com>
2022-04-03 14:44:35 -0300

    Creating integration tests: BookWriteRepository

---
+++ LearnCert.IntegrationTest/Domains/Book/Write/Repositories/BookWriteRepositoryTests.cs
@@ -0,0 +1,53 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using LearnCert.Domain.Domains.Book;
+using LearnCert.Domain.Infrastructure.Persistence;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace LearnCert.IntegrationTest.Domains.Book.Write.Repositories;
+
+public class BookWriteRepositoryTests : IntegrationTestBase
+{
+
+    private IBookWriteRepository _sut;
+
+    [SetUp]
+    public void BookWriteRepositoryTests_SetUp()
+    {
+        _sut = new BookWriteRepository(UnitOfWork, RegisterProviderService);
+    }
+    
+    [Test]
+    public void ShouldCreateBookState()
+    {
+        // Given
+        var book = Fixture.Create<BookAggregate>();
+        
+        // When
+        _sut.Save(book);
+        var result = _sut.GetById(book.Id);
+
+        // Then
+        result.Id.Should().Be(book.Id);
+        result.GetState().Title.Should().Be(book.GetState().Title);
+    }
+    
+    [Test]
+    public void ShouldChangeBookState()
+    {
+        // Given
+        var book = Fixture.Create<BookAggregate>();
+        var previousTitle = book.GetState().Title;
+        book.GetState().Title = "hello new";
+        
+        // When
+        _sut.Save(book);
+        var result = _sut.GetById(book.Id);
+
+        // Then
+        result.Id.Should().Be(book.Id);
+        result.GetState().Title.Should().Be(book.GetState().Title);
+        previousTitle.Should().NotBe(result.GetState().Title);
+    }
+}
