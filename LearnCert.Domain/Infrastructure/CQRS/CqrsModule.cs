47906a6f37a881595137724ae9c310029cf1d9a5
mcosta21 <marcioc424@gmail.com>
2022-04-03 10:41:52 -0300

    Creating a CqrsModule and Registering the RegisterProviderService

---
+++ LearnCert.Domain/Infrastructure/CQRS/CqrsModule.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace LearnCert.Domain.Infrastructure.Persistence;
+
+public class CqrsModule : IDependencyInjection
+{
+    public void Compose(IServiceCollection services)
+    {
+        services.AddSingleton<IRegisterProviderService, RegisterProviderService>();
+    }
+}
