380b41a9c9992fd564fd65fb49843e1a86ffd070
mcosta21 <marcioc424@gmail.com>
2022-04-03 10:47:16 -0300

    Creating the BookAggregate

---
+++ LearnCert.Domain/Domains/Book/BookAggregate.cs
@@ -0,0 +1,31 @@
+﻿using LearnCert.Domain.Infrastructure.Persistence;
+
+namespace LearnCert.Domain.Domains.Book;
+
+public interface IBookAggregate : IAggregate<Book>
+{
+    void Change(ChangeBookCommand cmd);
+}
+
+public class BookAggregate: BaseAggregate<Book>, IBookAggregate
+{
+
+    public BookAggregate(Book state)
+    {
+        State = state;
+    }
+
+    public BookAggregate(CreateBookCommand cmd)
+    {
+        State = new Book
+        {
+            Id = cmd.Id,
+            Title = cmd.Title
+        };
+    }
+    
+    public void Change(ChangeBookCommand cmd)
+    {
+        State.Title = cmd.Title;
+    }
+}
