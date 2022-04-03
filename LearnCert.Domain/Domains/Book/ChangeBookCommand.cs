76ec318d37ae3d4b93ba2f30e7ed475d9f9810c8
mcosta21 <marcioc424@gmail.com>
2022-04-03 10:46:52 -0300

    Creating the CreateBookCommand and ChangeBookCommand

---
+++ LearnCert.Domain/Domains/Book/ChangeBookCommand.cs
@@ -0,0 +1,9 @@
+﻿using LearnCert.Domain.Infrastructure.CQRS;
+
+namespace LearnCert.Domain.Domains.Book;
+
+public class ChangeBookCommand : ICommand
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+}
---
+++ LearnCert.Domain/Domains/Book/CreateBookCommand.cs
@@ -0,0 +1,9 @@
+﻿using LearnCert.Domain.Infrastructure.CQRS;
+
+namespace LearnCert.Domain.Domains.Book;
+
+public class CreateBookCommand : ICommand
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+}
