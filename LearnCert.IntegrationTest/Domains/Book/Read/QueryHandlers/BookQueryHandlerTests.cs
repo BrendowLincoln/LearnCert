1b5f358a1b18f9c695fc8118d28a1b7c4624b468
mcosta21 <marcioc424@gmail.com>
2022-04-04 20:01:11 -0300

    Creating integration test: BookQueryHandler

---
+++ LearnCert.IntegrationTest/Domains/Book/Read/QueryHandlers/BookQueryHandlerTests.cs
@@ -0,0 +1,40 @@
+﻿using AutoFixture;
+using FluentAssertions;
+using LearnCert.Domain.Domains.Book;
+using LearnCert.Domain.Domains.Book.Read.QueryHandlers;
+using NUnit.Framework;
+
+namespace LearnCert.IntegrationTest.Domains.Book.Read.QueryHandlers;
+
+public class BookQueryHandlerTests : IntegrationTestBase
+{
+
+    private IBookQueryHandler _sut;
+
+    [SetUp]
+    public void BookQueryHandlerTests_SetUp()
+    {
+        _sut = new BookQueryHandler(UnitOfWork);
+    }
+
+    [TestCase(true)]
+    [TestCase(false)]
+    public void ShouldVerifyIfBookExistsByTitle(bool exists)
+    {
+        // Given
+        var books = Fixture.CreateMany<BookState>().ToList();
+        var title = "Hello";
+        
+        if (exists)
+        {
+            books[2].Title = title;
+            UnitOfWork.Merge(books[2]);
+        }
+        
+        // When
+        var result = _sut.Exists(title);
+
+        // Then
+        result.Should().Be(exists);
+    }
+}
