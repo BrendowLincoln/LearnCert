19ee8460872a3577ad4a38db0f6e8c9965073495
mcosta21 <marcioc424@gmail.com>
2022-04-04 22:37:34 -0300

    Creating CertificationWriteRepository

---
+++ LearnCert.Domain/Domains/Certification/Write/Repositories/CertificationWriteRepository.cs
@@ -0,0 +1,22 @@
+﻿using LearnCert.Domain.Domains.Certification.Write.Aggregates;
+using LearnCert.Domain.Domains.Certification.Write.States;
+using LearnCert.Domain.Infrastructure.Persistence;
+
+namespace LearnCert.Domain.Domains.Certification.Write.Repositories;
+
+public interface ICertificationWriteRepository : IBaseWriteRepository<CertificationState, CertificationAggregate>
+{
+    
+}
+
+public class CertificationWriteRepository : BaseWriteRepository<CertificationState, CertificationAggregate>, ICertificationWriteRepository
+{
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IRegisterProviderService _registerProviderService;
+    
+    public CertificationWriteRepository(IUnitOfWork unitOfWork, IRegisterProviderService registerProviderService) : base(unitOfWork, registerProviderService)
+    {
+        _unitOfWork = unitOfWork;
+        _registerProviderService = registerProviderService;
+    }
+}
