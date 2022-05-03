2268d3d23108e2e31b9f01aeb7c708f8c909a468
mcosta21 <marcioc424@gmail.com>
2022-05-02 21:27:54 -0300

    Creating CreateQuestionDescriptionView with migration

---
+++ LearnCert.Domain/Infrastructure/Persistence/Migrations/20220501_CreateQuestionDescriptionView.cs
@@ -0,0 +1,17 @@
+﻿using FluentMigrator;
+using LearnCert.Domain.Infrastructure.Persistence;
+
+namespace LearnCert.Domain.Infraestructure.Persistence.Migrations;
+
+[Migration(20220501)]
+public class CreateQuestionDescriptionView  : Migration
+{
+    public override void Up()
+    {
+        Execute.Script("../LearnCert.Domain/Infrastructure/Persistence/Migrations/Scripts/20220501_CreateQuestionDescriptionView.sql");
+    }
+
+    public override void Down()
+    {   
+    }
+}
---
+++ LearnCert.Domain/Infrastructure/Persistence/Migrations/Scripts/20220501_CreateQuestionDescriptionView.sql
@@ -0,0 +1,13 @@
+﻿CREATE OR REPLACE VIEW QuestionDescriptionView as
+SELECT
+    qd.Id as Id,
+    CONCAT(m.Code, '.', q.Code) as Code,
+    qd.Description as Description,
+    qd.LanguageType as LanguageType,
+    m.CertificationId as CertificationId,
+    m.Title as ModuleTitle,
+    q.Id as QuestionId
+FROM QuestionDescription qd
+         INNER JOIN Question q on q.Id = qd.QuestionId
+         INNER JOIN Module m on m.Id = q.ModuleId
+    
