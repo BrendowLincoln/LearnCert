c86db9265355e4ecf26eb74ae14acde804b88832
mcosta21 <marcioc424@gmail.com>
2022-05-02 21:25:18 -0300

    Fix LanguageType enum map on QuestionDescriptionStateMap using AsEnumString

--- LearnCert.Domain/Domains/Certification/Write/Maps/QuestionDescriptionStateMap.cs
+++ LearnCert.Domain/Domains/Certification/Write/Maps/QuestionDescriptionStateMap.cs
@@ -1,6 +1,7 @@
 ﻿using FluentNHibernate.Mapping;
 using LearnCert.Domain.Domains.Certification.Write.Enums;
 using LearnCert.Domain.Domains.Certification.Write.States;
+using LearnCert.Domain.Infrastructure.Persistence;
 
 namespace LearnCert.Domain.Domains.Certification.Write.Maps;
 
@@ -12,10 +13,9 @@ public class QuestionDescriptionStateMap : ClassMap<QuestionDescriptionState>
             
         Id(x => x.Id).GeneratedBy.Assigned();
         Map(x => x.Description);
-        Map(x => x.LanguageType).CustomType<LanguageType>();
+        Map(x => x.LanguageType).AsEnumString<LanguageType>();
 
         References(x => x.Question, "QuestionId");
-
         
         HasMany(x => x.AnswerOptions)
             .KeyColumn("QuestionDescriptionId")
