c7a38f25c4420307d79890640d91c2725e7cf10e
mcosta21 <marcioc424@gmail.com>
2022-05-02 21:24:28 -0300

    Creating EnumStringType Extension

---
+++ LearnCert.Domain/Infrastructure/Persistence/FluentNHibernateExtensions.cs
@@ -0,0 +1,14 @@
+﻿using FluentNHibernate.Mapping;
+using NHibernate.Type;
+
+namespace LearnCert.Domain.Infrastructure.Persistence;
+
+public static class FluentNHibernateExtensions
+{
+
+    public static PropertyPart AsEnumString<TEnum>(this PropertyPart propertyPart)
+    {
+        return propertyPart.CustomType<EnumStringType<TEnum>>();
+    }
+    
+}
